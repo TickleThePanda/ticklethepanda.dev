925e08b2937cdd3fe87c308b728f2c150cb50abc
Thomas 'Panda' Attwood <thomas.pg.attwood@gmail.com>
2016-05-07 12:55:35 +0100

    updating style

--- www/page-layout.php
+++ www/page-layout.php
@@ -3,34 +3,29 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=0.8">
     <link href="default.css" rel="stylesheet" type="text/css">
-    <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-51822811-1', 'ticklethepanda.co.uk');
-  ga('send', 'pageview');
-
-    </script>
     <title><?php if(isset($TPL->PageTitle)) { echo $TPL->PageTitle; } ?> - ticklethepanda</title>
   </head>
   <body>
     <div id="main-container">
       <div id="infobar">
         <div id="header">
-          <a id="title" href="http://ticklethepanda.co.uk/">
-            ticklethepanda  
+          <a id="title" href="https://www.ticklethepanda.co.uk/">
+            ticklethepanda
           </a>
-          <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
-            viewBox="0 0 9 7" preserveAspectRatio="xMidYMid slice" id="nav-toggle">
+          <svg
+            id="nav-toggle"
+            xmlns="http://www.w3.org/2000/svg" version="1.1"
+            viewBox="0 0 9 7" preserveAspectRatio="xMidYMid slice">
             <path
+               class="animated-line"
                style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
                d="m 1.5,2 6,0"/>
             <path
+               class="animated-line"
                style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
                d="m 1.5,4 6,0"/>
             <path
+               class="animated-line"
                style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
                d="m 1.5,6 6,0"/>
           </svg>
@@ -76,5 +71,14 @@
       </div>
     </div>
     <script src="scripts/page.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-51822811-1', 'ticklethepanda.co.uk');
+      ga('send', 'pageview');
+    </script>
   </body>
 </html>
