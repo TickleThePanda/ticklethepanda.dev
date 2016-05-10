2bef867f96164b09eeb3315921cb026acb492283
TickleThePanda <thomas.pg.attwood@gmail.com>
2016-05-10 22:49:31 +0100

    adding image of me

--- public/content/about.php
+++ public/content/about.php
@@ -11,6 +11,7 @@ if (!isset($TPL)) {
 }
 ?>
 <h3>Meet Panda</h3>
+<img src="/images/content/me.png">
 <p class="first-paragraph">Hi, my name is Thomas 'Panda' Attwood. TickleThePanda is a pseudonym that I use while working online. I am currently studying Computer Science (BSc) at Royal Holloway, University of London. I love writing software that presents data in unique and interesting ways and I also enjoy web design and photography.</p>
 <p><a class="github-link" href="https://github.com/TickleThePanda"><img src="images/social/github.png"></img>TickleThePanda on GitHub</a></p>
 <p><a href="https://twitter.com/ticklethepanda" class="twitter-follow-button" data-show-count="false">Follow @ticklethepanda</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></p>
