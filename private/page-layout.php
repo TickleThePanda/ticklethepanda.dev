71f2559cfe595ee493051cb2cb187aa58486e183
TickleThePanda <thomas.pg.attwood@gmail.com>
2016-05-10 20:55:59 +0100

    removing unrequired '#main-container'

--- private/page-layout.php
+++ private/page-layout.php
@@ -6,66 +6,64 @@
     <title><?php if(isset($TPL->PageTitle)) { echo $TPL->PageTitle; } ?> - ticklethepanda</title>
   </head>
   <body>
-    <div id="main-container">
-      <div id="infobar">
-        <div id="header">
-          <a id="title" href="https://www.ticklethepanda.co.uk/">
-            ticklethepanda
-          </a>
-          <svg
-            id="nav-toggle"
-            xmlns="http://www.w3.org/2000/svg" version="1.1"
-            viewBox="0 0 9 7" preserveAspectRatio="xMidYMid slice">
-            <path
-               class="animated-line"
-               style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-               d="m 1.5,2 6,0"/>
-            <path
-               class="animated-line"
-               style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-               d="m 1.5,4 6,0"/>
-            <path
-               class="animated-line"
-               style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-               d="m 1.5,6 6,0"/>
-          </svg>
-        </div>
-        <div id="nav">
-          <div class="sidebar-title">info</div>
-          <div class="sidebar-line"></div>
-          <div class="sidebar-link"><a href="?action=home">home</a></div>
-          <div class="sidebar-link"><a href="?action=about">about</a></div>
-          <div class="sidebar-title">projects</div>
-          <div class="sidebar-line"></div>
-          <div class="sidebar-link"><a href="?action=messages">messages book</a></div>
-          <div class="sidebar-link"><a href="?action=location">location map</a></div>
-          <div class="sidebar-link"><a href="?action=health">health tracking</a></div>
-          <div class="sidebar-link"><a href="?action=photography">photography</a></div>
-          <div class="sidebar-line no-title"></div>
-          <div id="biog">
-            <br>
-            hey, i'm <span class="v-bold">panda</span>!<br><br>
-            i'm a <span class="v-bold">geek</span> who studied<br>
-            <span class="v-bold">computer science</span><br><br>
-            i work as a <span class="v-bold">software<br>engineer</span> at stfc<br><br>
-            i love playing<br>
-            with <span class="v-bold">data</span> and<br>
-            presenting it in<br>
-            different ways<br><br>
-            i've created this<br>
-            space to show my<br>
-            recent <span class="v-bold">projects</span><br><br>
-          </div>
+    <div id="infobar">
+      <div id="header">
+        <a id="title" href="https://www.ticklethepanda.co.uk/">
+          ticklethepanda
+        </a>
+        <svg
+          id="nav-toggle"
+          xmlns="http://www.w3.org/2000/svg" version="1.1"
+          viewBox="0 0 9 7" preserveAspectRatio="xMidYMid slice">
+          <path
+              class="animated-line"
+              style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+              d="m 1.5,2 6,0"/>
+          <path
+              class="animated-line"
+              style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+              d="m 1.5,4 6,0"/>
+          <path
+              class="animated-line"
+              style="fill:none;fill-rule:evenodd;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+              d="m 1.5,6 6,0"/>
+        </svg>
+      </div>
+      <div id="nav">
+        <div class="sidebar-title">info</div>
+        <div class="sidebar-line"></div>
+        <div class="sidebar-link"><a href="?action=home">home</a></div>
+        <div class="sidebar-link"><a href="?action=about">about</a></div>
+        <div class="sidebar-title">projects</div>
+        <div class="sidebar-line"></div>
+        <div class="sidebar-link"><a href="?action=messages">messages book</a></div>
+        <div class="sidebar-link"><a href="?action=location">location map</a></div>
+        <div class="sidebar-link"><a href="?action=health">health tracking</a></div>
+        <div class="sidebar-link"><a href="?action=photography">photography</a></div>
+        <div class="sidebar-line no-title"></div>
+        <div id="biog">
+          <br>
+          hey, i'm <span class="v-bold">panda</span>!<br><br>
+          i'm a <span class="v-bold">geek</span> who studied<br>
+          <span class="v-bold">computer science</span><br><br>
+          i work as a <span class="v-bold">software<br>engineer</span> at stfc<br><br>
+          i love playing<br>
+          with <span class="v-bold">data</span> and<br>
+          presenting it in<br>
+          different ways<br><br>
+          i've created this<br>
+          space to show my<br>
+          recent <span class="v-bold">projects</span><br><br>
         </div>
       </div>
-      <div id="content-wrapper">
-        <div id="content-scroll-wrap">
-          <div id="subtitle">
-             &gt;&nbsp;<?php if(isset($TPL->PageTitle)) { echo $TPL->PageTitle; } ?>
-          </div>
-          <div id="content">
-            <?php if(isset($TPL->ContentBody)) { include $TPL->ContentBody; } ?>
-          </div>
+    </div>
+    <div id="content-wrapper">
+      <div id="content-scroll-wrap">
+        <div id="subtitle">
+            &gt;&nbsp;<?php if(isset($TPL->PageTitle)) { echo $TPL->PageTitle; } ?>
+        </div>
+        <div id="content">
+          <?php if(isset($TPL->ContentBody)) { include $TPL->ContentBody; } ?>
         </div>
       </div>
     </div>
