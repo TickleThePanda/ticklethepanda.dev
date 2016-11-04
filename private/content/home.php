1f54271d036d64a48350c5a149e0816c5a600c1f
Thomas 'Panda' Attwood <ticklethepanda@gmail.com>
2016-11-04 16:54:14 +0000

    adding experience

--- private/content/home.php
+++ private/content/home.php
@@ -13,6 +13,21 @@ if (!isset($TPL)) {
 <img id="my-face" src="/images/content/me.png">
 <h3>Meet Panda</h3>
 <p class="first-paragraph">Hi, my name is Thomas 'Panda' Attwood. TickleThePanda is a pseudonym that I use while working online. I studied Computer Science at Royal Holloway, Universty of London. I'm now working as a full stack web developer at STFC; the main technologies that I work with are Java (and JavaEE), C#, HTML5, CSS3, and JavaScript. I love writing software that presents data in unique and interesting ways and I also enjoy web design and photography.</p>
+<h3>Experience</h3>
+<div class="experiences">
+  <div class="experience">
+    <div class="role">Graduate Software Engineer</div>
+    <div class="dates">August 2015 - Present</div>
+    <div class="place">Science and Technology Facilities Council</div>
+    <div class="description">Designing, developing, testing, maintaining, and providing support for web applications. Experience managing and supervising an individual.</div>
+  </div>
+  <div class="experience">
+    <div class="role">BSc Computer Science (Hons) - First Class</div>
+    <div class="dates">2012-2015</div>
+    <div class="place">Royal Holloway, University of London</div>
+  </div>
+</div>
+
 <div id="social-media-widget">
   <div class="social-media-link">
     <a href='https://twitter.com/ticklethepanda'>
@@ -30,4 +45,5 @@ if (!isset($TPL)) {
     </a>
   </div>
 </div>
+
 
