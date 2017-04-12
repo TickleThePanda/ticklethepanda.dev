7bc138884fad6b30d22931e4b5a53c39ca4dbeb7
Thomas 'Panda' Attwood <ticklethepanda@gmail.com>
2017-04-12 19:11:47 +0000

    hide nav on click anywhere

--- public/scripts/page.js
+++ public/scripts/page.js
@@ -1,10 +1,30 @@
 (function() {
-  document.getElementById("nav-toggle").addEventListener("click", function(event) {
-    var nav = document.getElementById("nav");
+  function getNav() {
+    return document.getElementById("sidebar-content");
+  }
+
+  function closeNav(nav) {
+    nav.className = nav.className.replace(/\bopen-nav\b/,'');
+  };
+
+  document.getElementById("nav-toggle")
+      .addEventListener("click", function(event) {
+    var nav = getNav();
     if (nav.classList.contains("open-nav")) {
-      nav.className = nav.className.replace(/\bopen-nav\b/,'');
+      closeNav(nav);
     } else {
       nav.className = nav.className + "open-nav";
+    }
+  });
+
+  document.addEventListener("click", function(event) {
+    let target = event.target || event.srcElement;
+
+    let navToggle = document.getElementById("nav-toggle");
+
+    if (!navToggle.isSameNode(target) 
+        && !navToggle.contains(target)) {
+      closeNav(getNav());
     }
   });
 
