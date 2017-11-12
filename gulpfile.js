c9f87fe4ed4cef80294084dd4b10de858e519693
Thomas 'Panda' Attwood <ticklethepanda@gmail.com>
2017-11-12 22:46:36 +0000

    Convert to static site
    Use Jekyll for static site stuff. Remove admin functionality
    because that will be done later.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,45 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var cleanCss = require('gulp-clean-css');
-var uglify = require('gulp-uglify');
+var uglifyjs = require('uglify-es');
+var composer = require('gulp-uglify/composer');
 var sourcemaps = require('gulp-sourcemaps');
+
+var minify = composer(uglifyjs, console);
+
+let assetsBaseOutput = 'assets';
 
 gulp.task('css', function() {
   return gulp.src('_less/[^_]*.less')
     .pipe(less())
     .pipe(cleanCss({ skipAggressiveMerging: true }))
-    .pipe(gulp.dest('public/style/'));
+    .pipe(gulp.dest(assetsBaseOutput + '/style/'));
 });
 
 gulp.task('js', function() {
   return gulp.src('_js/**/*.js')
     .pipe(sourcemaps.init())
-      .pipe(uglify())
+      .pipe(minify())
+      .on('error', err => console.log('uglify error', err))
     .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest('public/scripts/'));
+    .pipe(gulp.dest(assetsBaseOutput + '/scripts/'));
 });
 
 gulp.task('vega', function() {
   return gulp.src('_vg/**/*.json')
-    .pipe(gulp.dest('public/vega/'));
+    .pipe(gulp.dest(assetsBaseOutput + '/vega/'));
 });
 
-gulp.task('default', [ 'css', 'js', 'vega' ]);
+gulp.task('images', function () {
+  gulp.src('_images/**/*')
+    .pipe(gulp.dest(assetsBaseOutput + '/images/'));
+});
+
+gulp.task('default', [ 'css', 'js', 'vega', 'images' ]);
 
 gulp.task('watch', function() {
   gulp.watch('_js/**/*.js', ['js']);
   gulp.watch('_less/**/*.less', ['css']);
   gulp.watch('_vg/**/*.json', ['vega']);
+  gulp.watch('_images/**/*', ['images']);
 });
