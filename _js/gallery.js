541b3b309d4500600fd9b2fa78c92ee3259c7681
Thomas 'Panda' Attwood <ticklethepanda@gmail.com>
2017-12-27 23:38:24 +0000

    convert gallery to statically generated

--- _js/gallery.js
+++ _js/gallery.js
@@ -18,7 +18,6 @@
     let parameters = getHashParameters();
 
     let galleryParameter = parameters['gallery'];
-    console.log(galleryParameter);
     if (galleryParameter) {
       let galleries = document.getElementsByClassName('gallery-container');
       if(galleries.length > 0) {
@@ -37,55 +36,6 @@
     }
   }
 
-  function loadGalleries() {
-    return fetch(ENV.apiBaseUrl + '/gallery')
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function(result) {
-        let thumbsPath = result.thumbsPath;
-        let fullPath = result.fullPath;
-
-        let galleries = result.galleries.map((gallery, i) => {
-          let navGenerator = () => {
-            let nav = '';
-            if(i !== 0) {
-              nav += `<a href='#gallery=${ result.galleries[i - 1].reference }' class='prev'>&lt;-- prev</a>&nbsp;`
-            }
-            if(i !== result.galleries.length -1) {
-              nav += `<a href='#gallery=${ result.galleries[i + 1].reference }' class='next'>next --&gt;</a>`
-            }
-            return nav;
-          };
-          let imagesGenerator = () => {
-            return gallery.images.map((image) => {
-              return `
-                <a class='${ image.favourite ? 'favourite' : '' }'
-                    href='${ENV.assetsBaseUrl}${fullPath}${gallery.reference}/${image.fileName}'>
-                  <img src='${ENV.assetsBaseUrl}${ image.favourite ? fullPath : thumbsPath }${ gallery.reference }/${ image.fileName }'>
-                </a>`;
-            }).join('');
-          }
-          return `
-            <div id='gallery-${gallery.reference}' class='gallery-container'>
-              <div class='gallery-nav'>
-                ${ navGenerator() }
-              </div>
-              <h2>${ gallery.name }</h2>
-              <p>${ gallery.description }</p>
-              <div class='gallery'>
-                ${ imagesGenerator() }
-              </div>
-            </div>`;
-        });
-
-        let galleriesElement = document.getElementById('galleries');
-        galleries.forEach((gallery) => {
-          galleriesElement.innerHTML += gallery;
-        });
-      });
-  }
-
-  window.onhashchange = showSelectedGallery;
-  loadGalleries().then(showSelectedGallery);
+  window.addEventListener('hashchange', showSelectedGallery);
+  window.addEventListener('load', showSelectedGallery);
 })();
