1c696606d5928f90d30fe4503a11065b0912a6fd
Thomas 'Panda' Attwood <ticklethepanda@gmail.com>
2017-12-31 10:51:53 +0000

    use browser state istead of query strings for gallery

--- _js/gallery.js
+++ _js/gallery.js
@@ -1,41 +1,63 @@
-(function() {
-  function getHashParameters() {
-    var hashKeyValues = location.hash.substring(1).split('&');
-    var parameters = {};
-    hashKeyValues.map(function(kvPair) {
-      var pair = kvPair.split('=');
-      var key = pair[0];
-      var value = pair[1];
-      return { key: key,
-          value: value };
-    }).forEach(function(pair) {
-      parameters[pair.key] = pair.value;
-    });
-    return parameters;
-  }
+(function initGallery() {
 
-  function showSelectedGallery(e) {
-    let parameters = getHashParameters();
-
-    let galleryParameter = parameters['gallery'];
-    if (galleryParameter) {
-      let galleries = document.getElementsByClassName('gallery-container');
-      if(galleries.length > 0) {
-        Array.prototype.forEach.call(galleries, function(element) {
-          element.style.display = 'none';
-        });
-
-        let selectedGallery = document.getElementById('gallery-' + galleryParameter);
-        if (selectedGallery) {
-          selectedGallery.style.display = 'block';
-        } else {
-          galleries[0].style.display = 'block';
-        }
-      }
-
+  function registerDomLoadedEvent(cb) {
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', cb);
+    } else {
+      cb();
     }
   }
 
-  window.addEventListener('hashchange', showSelectedGallery);
-  window.addEventListener('load', showSelectedGallery);
+  registerDomLoadedEvent(makeGalleryLinksPushState);
+  registerDomLoadedEvent(showSelectedGallery);
+
+  window.addEventListener('load', loadImages);  
+
+  window.addEventListener('popstate', showSelectedGallery);
+  
+  function getSelectedGalleryId() {
+    let params = new URLSearchParams(document.location.search.substring(1));
+    return 'gallery-' + params.get('gallery');
+  }
+
+  function showSelectedGallery() {
+    let selectedGalleryId = getSelectedGalleryId();
+
+    let galleryElements = document.querySelectorAll('.gallery');
+    let galleryElementIds = Array.from(galleryElements).map(el => el.id);
+
+    galleryElements.forEach(el => {
+      el.classList.remove('selected');
+    });
+
+    if (selectedGalleryId && galleryElementIds.includes(selectedGalleryId)) {
+
+      let selectedGalleryElement = document.getElementById(selectedGalleryId);
+
+      selectedGalleryElement.classList.add('selected');
+    } else {
+      galleryElements[0].classList.add('selected');
+    }
+  }
+
+  function makeGalleryLinksPushState() {
+    let galleryNavLinkElements = document.querySelectorAll('.gallery nav a');
+
+    galleryNavLinkElements.forEach(el => {
+      el.addEventListener('click', e => {
+        e.preventDefault();
+        window.history.pushState(null, null, el.href)
+        showSelectedGallery();
+      }, true);
+    });
+
+  }
+
+  function loadImages() {
+    document.querySelectorAll('img[data-src]').forEach(img => {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.addEventListener('load', () => { img.removeAttribute('data-src') });
+    });
+  }
+
 })();
