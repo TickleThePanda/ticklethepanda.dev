43b5da592d1709a5f928528d9c8d643406303970
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2020-01-11 17:55:11 +0000

    Add cards

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+CSS.registerProperty({
+  name: '--random-border--safe-padding',
+  syntax: '<length-percentage>',
+  inherits: 'false',
+  initialValue: '0'
+});
+CSS.paintWorklet.addModule('/assets/scripts/worklet--random-background.js');
