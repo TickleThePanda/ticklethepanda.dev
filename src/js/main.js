6fccb4350c734d5169abe82c9bc6037b30cc56f2
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2020-01-14 19:31:48 +0000

    Generate random clip paths directly to enable transitions

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,114 @@
-CSS.registerProperty({
-  name: '--random-border--safe-padding',
-  syntax: '<length-percentage>',
-  inherits: 'false',
-  initialValue: '0'
-});
-CSS.paintWorklet.addModule('/assets/scripts/worklet--random-background.js');
+window.addEventListener('load', generateRandomCardClipPath);
+
+function generateRandomCardClipPath() {
+  const cards = document.querySelectorAll('.card');
+
+  for (let card of cards) {
+
+    const safeZone = getComputedStyle(card).getPropertyValue('--random-border--safe-padding');
+
+    /*
+     * We're going to generate points around each of the corners.
+     */
+    const cornerGenerationPoints = [
+      {
+        x: "0%",
+        y: "0%",
+        d: {
+          x: 1,
+          y: 1
+        },
+        d1: {
+          x: 0,
+          y: 1
+        },
+        d2: {
+          x: 1,
+          y: 0
+        }
+      },
+      {
+        x: "100%",
+        y: "0%",
+        d: {
+          x: -1,
+          y: 1
+        },
+        d1: {
+          x: -1,
+          y: 0
+        },
+        d2: {
+          x: 0,
+          y: 1
+        }
+      },
+      {
+        x: "100%",
+        y: "100%",
+        d: {
+          x: -1,
+          y: -1
+        },
+        d1: {
+          x: 0,
+          y: -1
+        },
+        d2: {
+          x: -1,
+          y: 0
+        }
+      },
+      {
+        x: "0%",
+        y: "100%",
+        d: {
+          x: 1,
+          y: -1
+        },
+        d1: {
+          x: 1,
+          y: 0
+        },
+        d2: {
+          x: 0,
+          y: -1
+        }
+      },
+    ]
+
+    const allPoints = [];
+
+    for (let corner of cornerGenerationPoints) {
+
+      if (Math.random() > 0.6) {
+
+        allPoints.push(`
+          calc(${corner.x} + ${corner.d1.x} * ${safeZone} + ${Math.random() * corner.d.x} * ${safeZone})
+          calc(${corner.y} + ${corner.d1.y} * ${safeZone} + ${Math.random() * corner.d.y} * ${safeZone})
+        `);
+        allPoints.push(`
+          calc(${corner.x} + ${corner.d2.x} * ${safeZone} + ${Math.random() * corner.d.x} * ${safeZone})
+          calc(${corner.y} + ${corner.d2.y} * ${safeZone} + ${Math.random() * corner.d.y} * ${safeZone})
+        `);
+      } else {
+        allPoints.push(`
+          calc(${corner.x} + ${Math.random() * corner.d.x} * ${safeZone})
+          calc(${corner.y} + ${Math.random() * corner.d.y} * ${safeZone})
+        `)
+      }
+
+    }
+
+    console.log(allPoints);
+
+    const clipPathValue = `polygon(${allPoints.join(',')})`;
+
+    console.log(clipPathValue);
+
+    card.style.clipPath = clipPathValue;
+
+    console.log(card.style);
+
+  }
+}
