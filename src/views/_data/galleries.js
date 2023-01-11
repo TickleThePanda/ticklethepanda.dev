928889cb298cc26d2a1a97249a2435449bf80bc4
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2023-01-11 17:09:02 +0000

    Add favourite gallery

--- src/views/_data/galleries.js
+++ src/views/_data/galleries.js
@@ -1,21 +1,39 @@
-const axios = require('axios');
+const axios = require("axios");
 
-module.exports = async function() {
-
-  const galleries = await axios.get('https://galleries.ticklethepanda.co.uk/', {
-    headers: {
-      "Cache-Control": "no-store"
+module.exports = async function () {
+  const galleriesResponse = await axios.get(
+    "https://galleries.ticklethepanda.co.uk/",
+    {
+      headers: {
+        "Cache-Control": "no-store",
+      },
     }
+  );
+
+  const galleriesData = galleriesResponse.data;
+
+  galleriesData.galleries.push({
+    name: "Favourites",
+    ref: "favourites",
+    description: "A collection of my favourites photographs that I've taken.",
+    images: galleriesData.galleries
+      .flatMap((g) => g.images)
+      .filter((i) => i.favourite)
+      .reverse(),
   });
 
-  printGalleryInfo(galleries.data);
+  printGalleryInfo(galleriesData);
 
-  return galleries.data;
-}
+  return galleriesData;
+};
 
 function printGalleryInfo(galleries) {
-  console.log("Galleries fetched:")
+  console.log("Galleries fetched:");
   for (let gallery of galleries.galleries) {
-    console.log(` - ${gallery.name} (${gallery.ref}) | ${gallery.images.map(i => i.name).join(', ')}`);
+    console.log(
+      ` - ${gallery.name} (${gallery.ref}) | ${gallery.images
+        .map((i) => i.name)
+        .join(", ")}`
+    );
   }
 }
