1d92bc1db9064d5181bcca0b5bc7615dd23a7d9e
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2025-04-17 00:15:14 +0100

    Add additional stats and improve titles

--- src/js/health.ts
+++ src/js/health.ts
@@ -4,11 +4,12 @@ import { ChartSizeManager } from "./lib/chart-size-manager";
 import { View, parse, Spec } from "vega";
 import { Handler } from "vega-tooltip";
 
+const TRYING_AGAIN_DATE = new Date(2022, 5, 1);
 
 type WeightChartOptions = {
   minDate: Date,
   source: BasicHistory[],
-  minY: number, 
+  minY: number,
   maxY: number
 }
 export class WeightChartManager {
@@ -17,9 +18,10 @@ export class WeightChartManager {
   private healthClient: HealthClient;
   private chartClient: ChartClient;
 
-  private chartOptions: Record<string, WeightChartOptions>| undefined;
+  private chartOptions: Record<string, WeightChartOptions> | undefined;
   private currentChart: string;
   private chartContainer: string;
+  private results: { weightWeek: BasicHistory[]; weightMonth: BasicHistory[]; weightQuarterlyMa: BasicHistory[]; } | undefined;
 
   constructor(healthClient: HealthClient, chartClient: ChartClient, currentChart: string, chartContainer: string) {
     this.healthClient = healthClient;
@@ -63,6 +65,7 @@ export class WeightChartManager {
       new ChartSizeManager().add(this.chart);
     }
 
+    this.addStats()
   }
 
   switchToChart(chart: string) {
@@ -79,25 +82,102 @@ export class WeightChartManager {
     }
   }
 
+  addStats() {
+
+    const calculateAvgWeeklyLoss = (
+      startDate: Date,
+    ) => {
+      const weightsInPeriod = this.results?.weightWeek.filter((d) => d.date > startDate);
+      weightsInPeriod?.sort((a, b) => a.date.getTime() - b.date.getTime());
+      
+      const firstWeightEntry = weightsInPeriod?.[0];
+      const lastWeightEntry = weightsInPeriod?.[weightsInPeriod.length - 1];
+      if (firstWeightEntry === undefined || lastWeightEntry === undefined) {
+        return "N/A";
+      }
+      const firstWeight = firstWeightEntry.weight;
+      const lastWeight = lastWeightEntry.weight;
+      const weightDifference = lastWeight - firstWeight;
+
+      const daysInPeriod = Math.floor((lastWeightEntry.date.getTime() - firstWeightEntry.date.getTime()) / (1000 * 60 * 60 * 24));
+      const weeksInPeriod = daysInPeriod / 7;
+      const avgWeeklyLoss = weightDifference / weeksInPeriod;
+
+      return {
+        avgLoss: -avgWeeklyLoss.toFixed(2),
+        startWeight: firstWeight,
+        endWeight: lastWeight
+      }
+    };
+
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    const avgWeeklyLossSinceTryingAgain = calculateAvgWeeklyLoss(TRYING_AGAIN_DATE);
+    const avgWeeklyLostOverTheLast2Years = calculateAvgWeeklyLoss(new Date(Date.now() - oneDay * 365 * 2));
+    const avgWeeklyLostOverTheLastYear = calculateAvgWeeklyLoss(new Date(Date.now() - oneDay * 365));
+    const avgWeeklyLostOverTheLastSixMonths = calculateAvgWeeklyLoss(new Date(Date.now() - oneDay * 180));
+    const avgWeeklyLostOverTheLastThreeMonths = calculateAvgWeeklyLoss(new Date(Date.now() - oneDay * 90));
+    const avgWeeklyLostOverTheLastMonth = calculateAvgWeeklyLoss(new Date(Date.now() - oneDay * 30));
+
+    const resultsElement = <HTMLElement>document.getElementById("weight-loss-summary");
+
+    const toElement = (title: string, info: "N/A" | { avgLoss: number, startWeight: number, endWeight: number}) => {
+      if (info == "N/A") {
+        return `
+          <div class="weight-loss-average">
+            <h4>${title}</h4>
+            <p>N/A <span class="unit">kg / week</span>
+          </div>
+        `
+      } else {
+        const {
+          avgLoss,
+          startWeight,
+          endWeight
+        } = info;
+        return `
+          <div class="weight-loss-average">
+            <h4>${title}</h4>
+            <p>${avgLoss} <span class="unit">kg / week</span><span class="initial-weight">(From ${startWeight.toFixed(0)} kg)</span>
+          </div>
+        `
+      }
+    }
+
+    resultsElement.innerHTML =
+      toElement("Since " + TRYING_AGAIN_DATE.toLocaleDateString("en-GB", { year: "numeric", month: "long" }), avgWeeklyLossSinceTryingAgain) +
+      toElement("1 year", avgWeeklyLostOverTheLastYear) +
+      toElement("3 months", avgWeeklyLostOverTheLastThreeMonths) +
+      toElement("1 month", avgWeeklyLostOverTheLastMonth);
+
+  }
+
   async reloadData() {
     await this.loadData()
     this.switchToChart(this.currentChart);
+    this.addStats();
   }
 
   private async loadData() {
 
     const [weightWeek, weightMonth, weightQuarterlyMa] = await Promise.all([
-      this.healthClient.fetchWeightHistoryWithPeriod(4, 4),
-      this.healthClient.fetchWeightHistoryWithPeriod(3, 10),
-      this.healthClient.fetchWeightHistoryWithPeriod(30, Math.floor(365 / 4 / 30))
+      this.healthClient.fetchWeightHistoryWithPeriod(1, 14),
+      this.healthClient.fetchWeightHistoryWithPeriod(1, 30),
+      this.healthClient.fetchWeightHistoryWithPeriod(1, 30*6)
     ]);
-  
+
     const dataMinDate = new Date(weightWeek[0].date.getTime());
     dataMinDate.setDate(dataMinDate.getDate() - 30);
     const sixMonthsAgo = new Date();
     sixMonthsAgo.setDate(sixMonthsAgo.getDate() - 30 * 6);
-  
-  
+
+    this.results = {
+      weightWeek: weightWeek,
+      weightMonth: weightMonth,
+      weightQuarterlyMa: weightQuarterlyMa
+    }
+
+
     const options: Record<string, WeightChartOptions> = {
       all: {
         minDate: dataMinDate,
@@ -105,8 +185,8 @@ export class WeightChartManager {
         minY: 75,
         maxY: 125
       },
-      "trying-again": { 
-        minDate: new Date(2022, 5, 1),
+      "trying-again": {
+        minDate: TRYING_AGAIN_DATE,
         source: weightMonth,
         minY: 75,
         maxY: 125
